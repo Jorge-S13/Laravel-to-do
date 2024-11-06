eb7b7990e6eefe1449ea41554f8bcbcab0132927
Jorge <elorgray@gmail.com>
2024-11-07 02:43:16 +0500

    add store method to ToDoController

--- app/Http/Controllers/ToDoController.php
+++ app/Http/Controllers/ToDoController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class ToDoController extends Controller
@@ -9,5 +11,14 @@ class ToDoController extends Controller
     public function index(): View
     {
         return view('todo.index');
+    }
+    public function store(Request $request): RedirectResponse
+    {
+        $validated = $request->validate([
+            'title' => 'required|string',
+        ]);
+        $request->user()->todos()->create($validated);
+
+        return redirect(route('to-do.index'));
     }
 }
