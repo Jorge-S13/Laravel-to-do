682c916cbdfea7bebb714c869d37c1abd7b90d8b
Jorge <elorgray@gmail.com>
2024-11-06 23:39:49 +0500

    todo/index.balde.php edited. add new form fields(title,description)

--- resources/views/todo/index.blade.php
+++ resources/views/todo/index.blade.php
@@ -2,13 +2,21 @@
     <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
         <form method="POST" action="{{ route('to-do.store') }}">
             @csrf
+            <label for="title" class="block text-sm/6 font-medium text-gray-900">Название</label>
+            <input type="text" name="title"
+                   class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm/6"
+                   placeholder="Введите название задачи"
+                   value="{{ old('title') }}">
+            <x-input-error :messages="$errors->get('title')" class="mt-2" />
+
+            <label for="description" class="block text-sm/6 font-medium text-gray-900">Описание</label>
             <textarea
-                name="message"
-                placeholder="{{ __('What\'s on your mind?') }}"
+                name="description"
+                placeholder="{{ __('Здесь вы можете добавить описание к задаче.') }}"
                 class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
-            >{{ old('message') }}</textarea>
-            <x-input-error :messages="$errors->get('message')" class="mt-2" />
-            <x-primary-button class="mt-4">{{ __('Chirp') }}</x-primary-button>
+            >{{ old('description') }}</textarea>
+            <x-input-error :messages="$errors->get('description')" class="mt-2" />
+            <x-primary-button class="mt-4">{{ __('Создать') }}</x-primary-button>
         </form>
     </div>
 </x-app-layout>
