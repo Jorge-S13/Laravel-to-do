7d926b5d9263ef6f754ed6ffbcc262f60cff9d9e
Jorge <elorgray@gmail.com>
2024-11-07 17:33:50 +0500

    Add home button to ToDo index.blade

--- resources/views/todo/index.blade.php
+++ resources/views/todo/index.blade.php
@@ -17,6 +17,7 @@
             >{{ old('description') }}</textarea>
             <x-input-error :messages="$errors->get('description')" class="mt-2" />
             <x-primary-button class="mt-4">{{ __('Создать') }}</x-primary-button>
+            <a href="{{route('dashboard')}}" class="'inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150'">На главную</a>
         </form>
     </div>
 </x-app-layout>
